14a3caf0eef40c8cb0a195f22ce57c55e1aa0784
areeman <andrew.reeman@gmail.com>
2020-05-15 00:26:15 +0100

    Can find negative numbers

---
+++ README.md
@@ -0,0 +1,1 @@
+44--4 is a good test case for token parsing
--- convert/converter.go
+++ convert/converter.go
@@ -2,6 +2,7 @@ package convert
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 )
 
@@ -27,27 +28,67 @@ import (
 
 // Convert the provided expression from infix to a postfix notation
 func Convert(expression string) string {
+	if len(expression) == 0 {
+		return expression
+	}
 
 	tokenize(expression)
 
-	return expression
+	return "Not implemented"
 }
 func tokenize(expression string) {
 	scanner := bufio.NewScanner(strings.NewReader(expression))
 
-	// for {
-	// 	if reader.Len() == 0 {
-	// 		fmt.Println("Finished reading")
-	// 		break
-	// 	}
+	tokens := make([]string, 0, len(expression))
 
-	// 	readRune, length, readError := reader.ReadRune()
+	split := func(remaining []byte, atEOF bool) (advance int, token []byte, err error) {
+		var negativeSign byte
+		for i, b := range remaining {
+			fmt.Println("Byte: ", b)
 
-	// 	if length == 0 || readError != nil {
-	// 		fmt.Println("Unexpectadly finished reading")
-	// 		break
-	// 	}
-	// 	fmt.Println("Read rune ", readRune)
-	// }
+			if isOperator(b) {
+				if isNegativeSign(b, tokens) {
+					negativeSign = b
+				} else {
+					return i + 1, remaining[i : i+1], nil
+				}
+			} else if isNumber(b) {
+				if negativeSign == '-' {
+					return i + 1, remaining[i-1 : i+1], nil
+				}
+				return i + 1, remaining[i : i+1], nil
+			} else if b == '(' || b == ')' {
+				return i + 1, remaining[i : i+1], nil
+			}
+		}
 
+		return len(remaining), nil, nil
+	}
+
+	scanner.Split(split)
+
+	for scanner.Scan() {
+		tokens = append(tokens, scanner.Text())
+	}
+
+	for _, t := range tokens {
+		fmt.Println("Token: ", t)
+	}
+}
+
+func isOperator(b byte) bool {
+	return b == '+' || b == '-' || b == '/' || b == '*'
+}
+
+func isNumber(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func isNegativeSign(b byte, currentTokens []string) bool {
+	lastItemIndex := len(currentTokens) - 1
+	if b == '-' && len(currentTokens) > 0 && len(currentTokens[lastItemIndex]) == 1 {
+		lastToken := currentTokens[lastItemIndex]
+		return !isNumber(lastToken[0])
+	}
+	return false
 }
